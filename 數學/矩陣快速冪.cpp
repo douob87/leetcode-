ab76df5bb501326e99845003f1adb56fd76ae575
douob87 <ja83067ob6@gmail.com>
2025-10-19 17:02:13 +0800

    qpow

--- 數學/矩陣快速冪.cpp
+++ 數學/矩陣快速冪.cpp
@@ -12,3 +12,30 @@ int qpow(int a, int n){
 }
 
 //矩陣版
+const int N = 2;
+int MOD;
+using Matrix = array<array<long long, N>, N>;
+Matrix mul(Matrix& a, Matrix& b){
+    Matrix c{};
+    for(int i=0;i<N;i++){
+        for(int k=0;k<N;k++){
+            if(a[i][k] == 0)continue;
+            for(int j=0;j<N;j++){
+                c[i][j] = (c[i][j] + a[i][k] * b[k][j] % MOD) % MOD;
+            }
+        }
+    }
+    return c;
+}
+Matrix qpow(Matrix& a, int n){
+    Matrix res{};
+    for(int i=0;i<N;i++)res[i][i] = 1;
+    while(n){
+        if(n & 1){
+            res = mul(res, a);
+        }
+        a = mul(a, a);
+        n >>= 1;
+    }
+    return res;
+}
