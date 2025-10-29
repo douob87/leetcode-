a1f1efca28de3756f5003e64dd98944a94afeabe
douob87 <ja83067ob6@gmail.com>
2025-10-29 11:32:03 +0800

    修正 最小質數篩

--- 數學/最小質數篩法.cpp
+++ 數學/最小質數篩法.cpp
@@ -1,20 +1,24 @@
-int MX = 1'000'01;
-vector<int> spf(MX, 0);
-iota(spf.begin(), spf.end(), 0);//把 spf[i] 設為自己
+#include <bits/stdc++.h>
+using namespace std;
 
-for(int i=2;i<MX;i++){
-    if(spf[i] != i)continue;//已經被標記
-    for(long long j=1LL*i*i;j<MX;j+=i){
-        if(spf[j] == j){//還沒被標記 就修改
-            spf[j] = i;
+int main(){
+    int MX = 1'000'01;
+    vector<int> spf(MX, 0);
+    iota(spf.begin(), spf.end(), 0);
+    for(int i=2;i<MX;i++){
+        //已經被標記
+        if(spf[i] != i)continue;
+        for(long long j=1LL*i*i;j<MX;j+=i){
+            //還沒被標記 就修改
+            if(spf[j] == j){
+                spf[j] = i;
+            }
         }
     }
-}
-
-//質因數分解 => 20 = 2 * 2 * 5
-int x = 2486;
-unordered_map<int, int> factor;
-while(x != 1){
-    factor[spf[x]] ++;
-    x /= spf[x];
+   //質因數分解 => 20 = 2 * 2 * 5
+    int x = 20;
+    while(x != 1){
+        cout<<spf[x]<<endl;
+        x /= spf[x];
+    } 
 }
