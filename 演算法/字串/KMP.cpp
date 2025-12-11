dbcbf0c115c7776e5272707fd86255548026c64d
douob87 <ja83067ob6@gmail.com>
2025-12-11 17:23:04 +0800

    新增KMP註解

--- 演算法/字串/KMP.cpp
+++ 演算法/字串/KMP.cpp
@@ -2,6 +2,16 @@
 int strStr(string text, string pattern) {
     int n = text.length(), m = pattern.length();
     vector<int> lcp(m, 0);
+    //lcp[i] 代表 在位置i 能匹配多少長度的前綴
+    //a b c a b a b
+    //0 0 0 1 2 1 2
+
+    //算法加速的關鍵 => j = lcp[j-1]
+    //a b c d a b c z a b c k
+    //0 0 0 0 1 2 3 0 1 2 3 0
+    //              j       i
+    //若 s[i], s[j]無法匹配，j不要直接從頭開始，可以掙扎一下。
+    //如果j不為0，代表s[j-1]匹配成功，lcp[j-1]的位置，就是可以掙扎的點。思考上面的範例。
     for(int i=1,j=0;i<m;i++){
         char b = pattern[i];
         while(j && b!=pattern[j])j = lcp[j-1];
