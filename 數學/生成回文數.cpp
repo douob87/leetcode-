77b22847d3a0ba0438de3f1c8bda90f95a2ca983
douob87 <ja83067ob6@gmail.com>
2025-12-11 17:09:29 +0800

    修改生成回文數模板

--- 數學/生成回文數.cpp
+++ 數學/生成回文數.cpp
@@ -1,21 +1,42 @@
-// 严格按顺序从小到大生成所有回文数（不用字符串转换）
-for (int base = 1; base <= 10000; base *= 10) {
-    // 生成奇数长度回文数
-    for (int i = base; i < base * 10; i++) {
-        int x = i;
-        for (int t = i / 10; t; t /= 10) {
-            x = x * 10 + t % 10;
-        }
-        pal.push_back(x);
-    }
-    // 生成偶数长度回文数
-    if (base <= 1000) {
-        for (int i = base; i < base * 10; i++) {
+#include <bits/stdc++.h>
+using namespace std;
+const int MX = 5000, BASE = 10;
+vector<int> pal;
+void generate_pal(){
+    pal = {0};//Dummy node
+    for(int p=1; ;p*=BASE){
+        //生成奇數回文
+        for(int i=p;i<p*BASE;i++){
             int x = i;
-            for (int t = i; t; t /= 10) {
-                x = x * 10 + t % 10;
+            for(int t=i/BASE;t;t/=BASE){
+                x = x*BASE + t%BASE;
+            }
+            if(x > MX){
+                pal.push_back(5005);
+                return ;
+            }
+            pal.push_back(x);
+        }
+        //生成偶數回文
+        for(int i=p;i<p*BASE;i++){
+            int x = i;
+            for(int t=i;t;t/=BASE){
+                x = x*BASE + t%BASE;
+            }
+            if(x > MX){
+                pal.push_back(5005);
+                return ;
             }
             pal.push_back(x);
         }
     }
+}
+int main(){
+    generate_pal();
+    //for(int p : pal)cout<<p<<" ";
+    /*
+    要找最近的回文數的話, 11, 22, 33, 44, 55, >= 31的value在22 33之間。
+    int j = lower_bound(pal.begin(), pal.end(), x) - pal.begin();
+    x 一定在 pal[j-1] ~ pal[j] 之間 
+    */
 }
