e8488580aae7af18c311cd77f0affb8a703decc6
douob87 <ja83067ob6@gmail.com>
2026-01-09 12:24:40 +0800

    ADD: 新增倍增算法(binary lifting)

--- 圖論/lca.cpp
+++ 圖論/lca.cpp
@@ -27,3 +27,37 @@ pair<TreeNode*, int> dfs(TreeNode* root){
     //return {d1==d2 ? root : d1>d2 ? t1 : t2, max(d1, d2)+1};
 }
 /***********************************************************/
+//倍增算法(Binary Lifting)
+class TreeAncestor {
+public:
+    vector<vector<int>> fa;
+    TreeAncestor(int n, vector<int>& parent) {
+        int m = log2(n) + 1;
+        fa.assign(n, vector<int>(m, -1));
+        //base case
+        for(int i=0;i<n;i++)fa[i][0] = parent[i];
+        //fa[i][j] 表示 第 i 個node 往上 2^j 是誰？
+        //fa[x][0] = parent[x]
+        //fa[x][1] = fa[fa[x][0]][0]
+        //轉移方程 : fa[x][i+1] = fa[fa[x][i]][i]
+        for(int i=0;i<m-1;i++){
+            for(int x=0;x<n;x++){
+                int p = fa[x][i];
+                if(p != -1){
+                    fa[x][i+1] = fa[p][i];
+                }
+            }
+        }
+    }
+    
+    int getKthAncestor(int node, int k) {
+        int m = log2(k) + 1;
+        for(int i=0;i<m;i++){
+            if(k>>i & 1){
+                node = fa[node][i];
+            }
+            if(node < 0)break;
+        }
+        return node;
+    }
+};
