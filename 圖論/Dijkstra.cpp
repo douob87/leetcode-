9f888182ceeae449f4f991e44b6a0f39c3c01a17
douob87 <ja83067ob6@gmail.com>
2026-01-15 20:41:24 +0800

    UPDATE: 修正Dijkstra模板

--- 圖論/Dijkstra.cpp
+++ 圖論/Dijkstra.cpp
@@ -1,25 +1,28 @@
+//找 *(0, 0) -> (n-1, m-1) 最大路徑總和
 //適用 邊權值>=0 
-//TC : O(|V+E| logE)
+//TC : O(|V+E|logE)
 using T = array<int, 3>;
-int n = mat.size(), m = mat[0].size();
+int n = grid.size(), m = grid[0].size();
 vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
 vector<int> dir = {1, 0, -1, 0, 1};
-queue<T> q;
-q.push({0, 0, 0});
-dist[0][0] = 0;
+priority_queue<T> pq;
+pq.push({grid[0][0], 0, 0});
+dist[0][0] = grid[0][0];
 
 while(!q.empty()){
-    auto [step, i, j] = q.front();q.pop();
-    if(step > dist[i][j])continue;//當前走過更少步 沒必要再走
+    auto [step, i, j] = pq.top();pq.pop();
+    if(step < dist[i][j])continue;//當前走過更少步 沒必要再走
     if(i==n-1 && j==m-1)return step; 
 
     for(int d=0;d<4;d++){
         int ni = i + dir[d];
         int nj = j + dir[d+1];
         if(ni>=0 && nj>=0 && ni<n && nj<m){
-            int new_step = step + path[ni][nj];
-            if(new_step < dist[ni][nj])
-                q.push({new_step, ni, nj});
+            int new_step = step + grid[ni][nj];
+            if(new_step > dist[ni][nj]){
+                dist[ni][nj] = new_step;
+                pq.push({new_step, ni, nj});
+            }
         }
     }
 }
