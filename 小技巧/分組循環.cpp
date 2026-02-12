4025905a641e48733dbed0b352fabce757b19d90
douob87 <ja83067ob6@gmail.com>
2026-02-12 11:04:12 +0800

    UPDATE: 分組循環

--- 小技巧/分組循環.cpp
+++ 小技巧/分組循環.cpp
@@ -1,7 +1,6 @@
 //111000011100011
 for(int i=0;i<n;){
     int j = i;
-    for(;j<n && s[i]==s[j];j++);
+    for(;i<n && s[i]==s[j];i++);
     //cout<<s[i]<<" "<<"長度 : "<<j-i+1<<endl;
-    i = j;
 }
