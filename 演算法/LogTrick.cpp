f1c616e8dcc8a6ec117f6d351635f2b48dc0f142
douob87 <ja83067ob6@gmail.com>
2026-03-22 21:24:40 +0800

    UPDATE: 更新LogTrick模板

--- 演算法/LogTrick.cpp
+++ 演算法/LogTrick.cpp
@@ -12,3 +12,29 @@ for(int i=0;i<n;i++){
         nums[j] = nums[i] | nums[j];
     }
 }
+
+void LogTrick(vector<int>& nums){
+    vector<pair<int, int>> or_left;
+    for(int i=0;i<nums.size();i++){
+        int x = nums[i];
+        for(auto& [or_val, _] : or_left){
+            or_val |= x;//添加 x 進去 ***自行修改***
+        }
+        or_left.push_back({x, i});
+
+        //去重
+        int m = 0;
+        for(int j=1;j<or_left.size();j++){
+            if(or_left[j].first != or_left[j-1].first){
+                or_left[++m] = or_left[j];
+            }
+        }
+        or_left.resize(m+1);
+
+        for(int k=0;k<=m;k++){
+            auto [or_val, left] = or_left[k];
+            int right = (k<m-1) ? or_left[k+1].second - 1 : i;
+            cout<<left<<" "<<right<<" "<<or_val<<endl;
+        }
+    }
+}
