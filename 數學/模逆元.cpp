00fda06afd69909cb1ae1d7fef43a0063309322b
douob87 <ja83067ob6@gmail.com>
2026-03-29 11:39:58 +0800

    UPDATE: 更新 LeetCode 模逆元模板

--- 數學/模逆元.cpp
+++ 數學/模逆元.cpp
@@ -1,11 +1,11 @@
 //(a/b) % p => 轉換成 a * b^-1(a 乘上 b的逆元)
 //b = qpow(b, MOD-2) % MOD, 費馬小定理...
-int MOD = 1'000'000'007;//1e9 + 7
-int MX = 41;
+const int MOD = 1'000'000'007;//1e9 + 7
+const int MX = 100001;
 int F[MX];
 int INV_F[MX];
 
-long long qpow(int a, int n){
+long long qpow(long long a, int n){
     long long res = 1;
     while(n){
         if(n & 1){
@@ -16,11 +16,13 @@ long long qpow(int a, int n){
     }
     return res;
 }
-
-F[0] = 1;
-for(int i=1;i<MX;i++)F[i] = i * F[i-1] % MOD;
-INV_F[MX-1] = qpow(F[MX-1], MOD-2);
-for(int i=MX-1;i;i--)INV_F[i-1] = INV_F[i] * i % MOD;//計算模逆元，1 / (1*2) = 1 / (1*2*3) * 3
+int init = [](){
+    F[0] = 1;
+    for(int i=1;i<MX;i++)F[i] = 1LL * i * F[i-1] % MOD;
+    INV_F[MX-1] = qpow(F[MX-1], MOD-2);
+    for(int i=MX-1;i;i--)INV_F[i-1] = 1LL * INV_F[i] * i % MOD;//計算模逆元，1 / (1*2) = 1 / (1*2*3) * 3
+    return 0;
+}();
 
 /*
 C(n, m) = n! / (m)! / (n-m)!
